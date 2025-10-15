68e857e0deec7960b46d61e44ef90c932ba4d097
Begzat Kamen <begzat.kamen@gmail.com>
2025-10-15 21:03:12 +0500

    5 day learn

---
+++ interger.py
@@ -0,0 +1,11 @@
+friends_qty  = 10
+print(f"I have {friends_qty} friends")
+print(type(friends_qty))
+
+any_number = float(input("Enter any number: "))
+print(f"You entered {any_number}, its type is {type(any_number)}")
+
+base = int(input("Enter base: "))
+power = int(input("Enter power: "))
+print(pow(base, power))
+print(base ** power)
