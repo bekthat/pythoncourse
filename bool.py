8e0a3a081bc27382723018ac7128f16211e11dd5
Begzat Kamen <begzat.kamen@gmail.com>
2025-10-15 21:27:31 +0500

    5 day learn

---
+++ bool.py
@@ -0,0 +1,13 @@
+is_authorized = True
+print(is_authorized)
+print(type(is_authorized))
+
+db_is_connected = False
+
+print(db_is_connected)
+print(type(db_is_connected))
+
+
+db_is_connected = True
+print(db_is_connected)
+
--- interger.py
+++ interger.py
@@ -1,4 +1,4 @@
-friends_qty  = 10
+friends_qty = 10
 print(f"I have {friends_qty} friends")
 print(type(friends_qty))
 
@@ -9,3 +9,14 @@ base = int(input("Enter base: "))
 power = int(input("Enter power: "))
 print(pow(base, power))
 print(base ** power)
+
+
+average_price = 99.99
+print(average_price)
+print(type(average_price))
+
+
+complex_a = 5 + 3j
+complex_b = 5 - 2j
+
+print(complex_a + complex_b)
